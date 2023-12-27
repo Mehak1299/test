1969e14f6a9aadc752a912192ed8b31354c4932c
Mehak1299 <154961648+Mehak1299@users.noreply.github.com>
2023-12-27 13:25:23 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        Scanner test = new Scanner(System.in);
+        test.nextLine();
+        Scanner test2 = new Scanner(System.in);
+        test2.nextLine();
+    }
+}
